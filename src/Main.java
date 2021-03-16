9e06f6b662a1a57f53f99e2a90a92a67b48c7cd1
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-03-16 16:40:45 +0300

    ReformatCode

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int currentBalance = 5000; // начальный счет
-        int refillAccount = 2200 ; // пополнение счёта
+        int refillAccount = 2200; // пополнение счёта
         int totalBalance;
         int totalBonus;
         if (refillAccount > 1000) {
-            totalBonus = refillAccount/100; // начисленный бонус
-            totalBalance = currentBalance+totalBonus+refillAccount;
-            System.out.println("Вы пополнили счёт на: " + refillAccount + ", у вас "+totalBonus+" бонусов. Итоговая сумма: "+totalBalance+" руб.");
-        }
-        else {
+            totalBonus = refillAccount / 100; // начисленный бонус
+            totalBalance = currentBalance + totalBonus + refillAccount;
+            System.out.println("Вы пополнили счёт на: " + refillAccount + ", у вас " + totalBonus + " бонусов. Итоговая сумма: " + totalBalance + " руб.");
+        } else {
             totalBalance = currentBalance + refillAccount;
             System.out.println("Вы пополнили счёт на: " + refillAccount + " руб., бонусов нет. Итоговая сумма:" + totalBalance + " руб.");
-            }
+        }
     }
 }
